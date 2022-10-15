642cfea09bd5710e04d1cfe95df232305bc58323
Curry346346 <65375919+ray346@users.noreply.github.com>
2022-10-16 07:31:11 +0800

    Add files via upload

--- mtr.js
+++ mtr.js
@@ -11,12 +11,22 @@ async function getInfo(){
     document.getElementById('message2').innerHTML=infoup2.route_no+ " 往 " +infoup2.dest_ch+" 的 "+infoup2.train_length+ "卡列車 <br>時間： " +infoup2.time_ch
     document.getElementById('message3').innerHTML=infoup22.route_no+ " 往 " +infoup22.dest_ch+" 的 "+infoup22.train_length+ "卡列車 <br>時間： " +infoup22.time_ch
     if(infoup1.time_ch == "即將抵達"){
-    document.getElementById('abc').innerHTML=infoup1.route_no+ " 往 " +infoup1.dest_ch+" 的 "+infoup1.train_length+ "卡列車即將即將抵達1號月台"
+    document.getElementById('abc').innerHTML=infoup1.route_no+ " 往 " +infoup1.dest_ch+" 的 "+infoup1.train_length+ "卡列車即將抵達1號月台"
+} else {
+    document.getElementById('abc').innerHTML=""
+}
+if(infoup12.time_ch == "即將抵達"){
+    document.getElementById('abc').innerHTML=infoup12.route_no+ " 往 " +infoup12.dest_ch+" 的 "+infoup12.train_length+ "卡列車即將抵達1號月台"
 } else {
     document.getElementById('abc').innerHTML=""
 }
 if(infoup2.time_ch == "即將抵達"){
-    document.getElementById('abc').innerHTML=infoup2.route_no+ " 往 " +infoup2.dest_ch+" 的 "+infoup2.train_length+ "卡列車即將即將抵達2號月台"
+    document.getElementById('abc').innerHTML=infoup2.route_no+ " 往 " +infoup2.dest_ch+" 的 "+infoup2.train_length+ "卡列車即將抵達2號月台"
+} else {
+    document.getElementById('abc').innerHTML=""
+}
+if(infoup22.time_ch == "即將抵達"){
+    document.getElementById('abc').innerHTML=infoup22.route_no+ " 往 " +infoup22.dest_ch+" 的 "+infoup22.train_length+ "卡列車即將抵達2號月台"
 } else {
     document.getElementById('abc').innerHTML=""
 }
